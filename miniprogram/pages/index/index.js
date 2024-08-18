6da290ffcf1c0bb19b315c1c07d2d7c7efb69546
xiaorang <15019474951@163.com>
2024-08-18 23:02:07 +0800

    feat(index): 初步实现首页会员授权登录和雪王币优惠券静态展示效果

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -7,12 +7,13 @@ Page({
   data: {
     swiperList: [],
     currentIndex: 0,
+    isLogin: false,
   },
 
   onSwiperChange(e) {
-    const { current } = e.detail
+    const { current: currentIndex } = e.detail
     this.setData({
-      currentIndex: current,
+      currentIndex,
     })
   },
 
@@ -30,39 +31,4 @@ Page({
   onLoad(options) {
     this._loadSwiperList()
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {},
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {},
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {},
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {},
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {},
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {},
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {},
 })
