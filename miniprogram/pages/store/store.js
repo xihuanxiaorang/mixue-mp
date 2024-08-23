75eeb8be350634c0657140e4e947abc7160d98d0
xiaorang <15019474951@163.com>
2024-08-23 22:55:15 +0800

    feat(store): 初步实现门店列表页面地图组件和门店列表静态效果
    包括map地图组件的引入和基本应用、底部可滚动区域的门店列表渲染

--- miniprogram/pages/store/store.js
+++ miniprogram/pages/store/store.js
@@ -6,46 +6,98 @@ Page({
     tabs: [
       { value: 'nearby', label: '附近门店' },
       { value: 'recent', label: '常去门店' },
+      { value: 'star', label: '收藏门店' },
     ],
+    longitude: 0,
+    latitude: 0,
+    markers: [
+      {
+        id: 1,
+        longitude: '113.032113',
+        latitude: 28.190751,
+        iconPath: '/assets/images/marker.png',
+        width: '53rpx',
+        height: '64rpx',
+      },
+    ],
+    mapContext: null,
+    collapsed: false,
+  },
+
+  /**
+   * 收起/展开地图
+   */
+  collapseMap() {
+    const collapsed = !this.data.collapsed
+    this.setData({
+      collapsed,
+    })
+  },
+
+  /**
+   * 搜索门店
+   */
+  searchStore() {
+    wx.chooseLocation({
+      success: (res) => {
+        const { longitude, latitude } = res
+        this.data.mapContext.moveToLocation({
+          longitude,
+          latitude,
+        })
+      },
+      fail: (err) => {
+        console.error(err)
+      },
+    })
+  },
+
+  /**
+   * 移动到当前用户所在坐标位置
+   */
+  moveToCurrentLocation() {
+    this.data.mapContext.moveToLocation()
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad(options) {},
+  onLoad(options) {
+    this.getLocation()
+    this.getMapContext()
+  },
 
   /**
-   * 生命周期函数--监听页面初次渲染完成
+   * 获取地图组件的上下文对象
    */
-  onReady() {},
+  getMapContext() {
+    wx.createSelectorQuery()
+      .select('#store-map')
+      .context((res) => {
+        this.setData({
+          mapContext: res.context,
+        })
+      })
+      .exec()
+  },
 
   /**
-   * 生命周期函数--监听页面显示
+   * 获取当前用户坐标位置
    */
-  onShow() {},
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {},
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {},
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {},
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {},
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {},
+  getLocation() {
+    wx.getLocation({
+      type: 'wgs84',
+      isHighAccuracy: true,
+      success: (res) => {
+        const { longitude, latitude } = res
+        this.setData({
+          longitude,
+          latitude,
+        })
+      },
+      fail: (err) => {
+        console.error(err)
+      },
+    })
+  },
 })
